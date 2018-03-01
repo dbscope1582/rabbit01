fc864db7d9d634e761e16aee350fcf051d86168c
Dieter Binkert <dbinkert@basel.scope.ch>
2018-03-01 14:39:42 +0100

    change the default rabbitMQ server

--- src/Constants.java
+++ src/Constants.java
@@ -1,5 +1,6 @@
 public class Constants {
-    public static String rabbitServer() {return "192.168.1.155";}
+    // naoned public static String rabbitServer() {return "192.168.1.155";}
+    public static String rabbitServer() {return "192.168.1.154";}
     public final static String QUEUE_NAME = "hello";
     public final static String Encoding="UTF-8";
     public final static String ExchangeName = "mnesys.events.findingAid";
